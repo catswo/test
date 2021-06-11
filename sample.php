980d4ecf86cd1935fcb42fe1cdbdd2868d859ff4
katsuo <isono@pa-dn.com>
2021-06-11 17:21:48 +0900

    ソースを追加

---
+++ sample.php
@@ -0,0 +1,16 @@
+<?php
+
+class Sample {
+
+  public function func1(){
+    echo 'func1';
+  }
+
+  public function func2(){
+    echo 'func2';
+  }
+
+  public function func3(){
+    echo 'func3';
+  }
+}
